f23bc2d21383339b3db6edfd1daf852e04e410f2
balramsingh54 <singhbalram054@gmail.com>
2022-09-16 09:46:22 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+
+const NavBar = () => {
+    return (
+        <div className='Nav'>
+            Balram
+        </div>
+    )
+}
+
+export default NavBar;
+
